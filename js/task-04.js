c422c06284090ce129ed77c064a6b9731f8b2b02
Magdalena Uszynska <magdalenauszynska1@gmail.com>
2023-11-06 08:50:21 +0100

    fourth task

---
+++ js/task-04.js
@@ -0,0 +1,14 @@
+let counterValue = 0
+const displayContent = document.querySelector('#value')
+const btnInc = document.querySelector('button[data-action="increment"]')
+const btnDec = document.querySelector('button[data-action="decrement"]')
+
+btnInc.addEventListener('click', () => {
+    counterValue--
+    displayContent.textContent = counterValue
+}
+)
+btnDec.addEventListener('click', () => {
+    counterValue++
+    displayContent.textContent = counterValue
+})
