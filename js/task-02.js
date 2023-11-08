9b2ef752b36b3d080c86f6f397cc0407ad116607
Magdalena Uszynska <magdalenauszynska1@gmail.com>
2023-11-08 21:53:24 +0100

    second task

--- js/task-02.js
+++ js/task-02.js
@@ -1,12 +1,13 @@
 const ingredients = ['Potatoes', 'Mushrooms', 'Garlic', 'Tomatos', 'Herbs', 'Condiments'];
 
-const ingredientsRef = document.querySelector('#ingredients');
+const listIngr = document.querySelector('#ingredients')
+const listArray = []
 
-const itemsRef = ingredients.map((ingredient) => {
-  const item = document.createElement('li');
-  item.textContent = ingredient;
-  item.classList.add('item');
-  return item;
-});
+ingredients.forEach(ingredient => {
+	const item = document.createElement('li')
+	item.className = "items"
+	item.textContent = ingredient
+	listArray.push(item)
+})
 
-ingredientsRef.append(...itemsRef);
+listIngr.append(...listArray)
